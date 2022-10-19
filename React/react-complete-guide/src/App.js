92a2080e0f3fa3815bfc338dc57a18beded1d356
sachin.kumar <sachin.kumar@sparxitsolutions.com>
2022-10-19 19:59:15 +0530

    check

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
     <div className="App">
       <h1>Let's Start</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
-
 export default App;
---
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+
+function ExpenseItem() {
+    return(
+        <div>
+        <h2>Hello</h2>
+        </div>
+    );
+}
+export default ExpenseItem;

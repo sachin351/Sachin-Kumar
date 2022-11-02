19a1c987f8f43e04b7915fc912f941c6575641c7
sachin.kumar <sachin.kumar@sparxitsolutions.com>
2022-11-02 19:50:48 +0530

    expense-item

--- React/react-complete-guide/src/components/ExpenseItem.js
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -1,8 +1,12 @@
-
+import './ExpenseItem.css';
 function ExpenseItem() {
-    return(
-        <div>
-        <h2>Hello</h2>
+    return (
+        <div className='expense-item'>
+            <div>March 28th 2021</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$294.67</div>
+            </div>
         </div>
     );
 }

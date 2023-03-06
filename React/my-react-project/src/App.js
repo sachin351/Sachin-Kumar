6bc0335dd11cc18dfc3a040e4d49f6c5efda84e6
tizo-vipulgoyal112 <[vipul.goyal@iratechnologies.com]>
2023-03-06 09:47:44 +0530

    new react project

---
+++ React/my-react-project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from './component/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's Start</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;

f3bfcb103d2187e3c774748c72de83a4cc4f04e0
Sachin Kumar <sachinkumarmwn35@gmail.com>
2023-04-11 21:46:03 +0530

    app-check

--- React/my-react-project/src/App.js
+++ React/my-react-project/src/App.js
@@ -2,30 +2,30 @@ import './App.css';
 import Expenses from './component/Expenses';
 
 function App() {
-  const expenses=[
+  const expenses = [
     {
-      id:'e1',
+      id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020,7,14),
+      date: new Date(2020, 7, 14),
     },
     {
-      id:'e2',
+      id: 'e2',
       title: 'New TV',
       amount: 799.49,
-      date: new Date(2021,2,12),
+      date: new Date(2021, 2, 12),
     },
     {
-      id:'e3',
+      id: 'e3',
       title: 'Car Insurence',
       amount: 294.67,
-      date: new Date(2021,2,28),
+      date: new Date(2021, 2, 28),
     },
     {
-      id:'e4',
+      id: 'e4',
       title: 'New Wooden',
       amount: 94.12,
-      date: new Date(2020,7,14),
+      date: new Date(2020, 7, 14),
     },
   ];
   return (

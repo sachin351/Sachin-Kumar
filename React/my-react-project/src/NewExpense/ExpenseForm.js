1cdc930a9ed41a1d29d5d71333064e3464bf71b8
Sachin Kumar <sachin.kumar@sparxitsolutions.com>
2023-04-20 19:33:05 +0530

    check

--- React/my-react-project/src/NewExpense/ExpenseForm.js
+++ React/my-react-project/src/NewExpense/ExpenseForm.js
@@ -33,20 +33,21 @@ const ExpenseForm = ({ onExpenseAdd }) => {
         event.preventDefault();
         onExpenseAdd(userInput)
     }
+    
     return (
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={userInput} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChnageHandler} />
+                    <input type="number" value={userInput} min="0.01" step="0.01" onChange={amountChnageHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChnageHandler} />
+                    <input type="date" value={userInput} min="2019-01-01" max="2022-12-31" onChange={dateChnageHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">

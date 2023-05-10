e4212d7e6c57cf07ec8e033afdec108852e62c45
Sachin Kumar <sachin.kumar@sparxitsolutions.com>
2023-05-10 19:25:18 +0530

    sdjnklsd

--- React/my-react-project/src/component/ExpensesFilter.js
+++ React/my-react-project/src/component/ExpensesFilter.js
@@ -1,16 +1,17 @@
 
  
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import './ExpensesFilter.css';
 
 const ExpensesFilter = () => {
+  // const [yearFilter, setYearFilter] = useState(expenses);
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange={(event) => console.log(event.target.value)}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>

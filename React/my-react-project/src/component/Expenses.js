cf571776fca09678a34129a13b834f8d12322cf1
Sachin Kumar <sachin.kumar@sparxitsolutions.com>
2023-07-14 19:25:50 +0530

    djsdj

--- React/my-react-project/src/component/Expenses.js
+++ React/my-react-project/src/component/Expenses.js
@@ -4,36 +4,20 @@ import './Expenses.css';
 function Expenses(props) {
   return (
     <div className='expenses'>
-      <ExpensesFilter filterByYear = {props.filterByYear}/>
-      {props.items.map((item => (
-        <ExpenseItem
-          key={item.id}
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-        />
+      <ExpensesFilter filterByYear={props.filterByYear} />
+      {props.items.length === 0 ?(
+        <p className="no-data">No data found!</p>) : (
+          props.items.map((item => (
+            <ExpenseItem
+              key={item.id}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
 
-      )))}
-      {/* <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[4].title}
-        amount={props.items[4].amount}
-        date={props.items[4].date}
-      ></ExpenseItem> */}
+          )))
+        )
+        }
     </div>
   );
 }
